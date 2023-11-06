fda765c42e17ef19658ade48a68f6f827fa1ce9c
Alikha <osterrachefurungarn@gmail.com>
2023-11-06 10:18:58 +0000

    /learn page

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -12,10 +12,15 @@ export default function Home() {
         <>
             <Navigation route={2} />
 
-            <section id="header" className="bg-white bg-anim bg-[url('/bg.svg')] bg-[length:280%] sm:bg-[length:200%] md:bg-[length:140%] lg:bg-cover w-full mt-16 sm:mt-24 px-10 lg:px-28 py-40 flex justify-center items-center flex-col text-center">
-                <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-[2.8rem] animate fadeInDown font-bold xl:leading-normal mb-4">Penasaran untuk <span className="text-yellow-300">ber etika baik</span>?</h1>
-                <p className="text-lg lg:text-xl xl:text-2xl animate fadeInDown max-w-lg xl:max-w-xl mb-12" style={{animationDelay: "0.2s"}}>Ayo, jadilah remaja yang <b>beretika baik</b> dalam menggunakan media sosial dan internet!</p>
-                <MulaiButton style={{animationDelay: "0.4s"}} />
+            
+        <section id="header" className="bg-[#F8F8F8] w-full mt-16 lg:mt-24 px-8 sm:px-32 xl:px-40 py-28 flex justify-center items-center flex-col-reverse lg:flex-row">
+                <div className="w-full lg:w-1/2 text-center lg:text-left">
+                    <h1 className="text-2xl md:text-3xl xl:text-[2.8rem] animate fadeInDown font-bold xl:leading-normal mb-4">Apa itu Etika Digital?</h1>
+                    <p className="text-lg lg:text-xl xl:text-2xl mb-12 animate fadeInDown" style={{animationDelay: "0.2s"}}><b className='font-bold'>Etika digital</b> yaitu sebuah prinsip dan aturan perilaku yang membimbing cara kita <b className='font-bold'>berinteraksi, berbagi informasi, dan berkomunikasi secara online</b> dengan sopan, bertanggung jawab, dan menghormati privasi dan hak orang lain. <b className='font-bold'>ethic</b><b className='font-bold text-[#FFD74A]'>ally</b> sendiri bertujuan untuk mengajarkan cara bertindak dengan <b className='font-bold'>bijaksana dan bermoral dalam dunia maya.</b></p>
+                    <MulaiButton style={{animationDelay: "0.4s"}} />
+                </div>
+                <div className="w-full lg:w-1/2 mb-16 lg:mb-0 flex justify-center items-center animate fadeInUp">
+                </div>
             </section>
 
             <Footer />
