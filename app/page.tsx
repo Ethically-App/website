59d897ef67f75baec72c2c287c86e3dce0cd5281
Royyan Zaydan <77264514+Kasehito@users.noreply.github.com>
2023-12-08 09:48:39 +0700

    a

--- app/page.tsx
+++ app/page.tsx
@@ -79,8 +79,8 @@ export default function Home() {
         className="bg-white bg-anim bg-[url('/bg.svg')] bg-[length:280%] sm:bg-[length:200%] md:bg-[length:140%] lg:bg-cover w-full px-10 lg:px-28 py-40 flex justify-center items-center flex-col text-center"
       >
         <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-[2.8rem] animate fadeInDown font-bold xl:leading-normal mb-4">
-          Penasaran untuk{" "}
-          <span className="text-yellow-300">ber etika baik</span>?
+          Penasaran untuk <span className="text-yellow-300">beretika baik</span>
+          ?
         </h1>
         <p
           className="text-lg lg:text-xl xl:text-2xl animate fadeInDown max-w-lg xl:max-w-xl mb-12"
